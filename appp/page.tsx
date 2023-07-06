81e65ee9d73c9cf27a61b3938cb5024170993c64
MustajabAhmed <mustajabahmed02@gmail.com>
2023-07-06 21:30:30 +0500

    Navbar Completed

--- appp/page.tsx
+++ appp/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '../components/ui/button'
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      {/* Hello World */}
     </div>
   )
 }
---
+++ src/views/Hero.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@/components/ui/badge"
+
+const Hero = () => {
+    return (
+        <section>
+            {/* Left Div */}
+            <div>
+                <Badge className="py-3 px-6 rounded-lg bg-blue-200 text-blue-700 ">Badge</Badge>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-6">
+                    Taxing Laughter: The Joke Tax Chronicles
+                </h1>
+
+            </div>
+
+            {/* Right Div */}
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+
+export default Hero
