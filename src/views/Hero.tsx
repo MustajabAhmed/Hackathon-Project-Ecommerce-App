5294ec103246d2969f3c924b0763d8e543d7f2fa
MustajabAhmed <mustajabahmed02@gmail.com>
2023-07-09 19:25:32 +0500

    Install the Sanity Package

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -13,9 +13,7 @@ const Hero = () => {
                     An Industrial Take on Streetwear
                 </h1>
                 <p className="leading-7 [&:not(:first-child)]:mt-6">
-                    Once upon a time, in a far-off land, there was a very lazy king who
-                    spent all day lounging on his throne. One day, his advisors came to him
-                    with a problem: the kingdom was running out of money.
+                    Anyone can beat you but no one can beat your outfit as long as you wear Dine outfits.
                 </p>
                 <Button className="bg-black text-white h-12 px-8 mt-4">
                     Start Shopping
